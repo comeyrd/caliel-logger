84e1014a8f6c4f70b54292918917f835223d9667
EYRAUD Come <come.eyraud@telecom-paris.fr>
2023-12-05 11:46:17 +0100

    Adding a reply feature

--- caliel.py
+++ caliel.py
@@ -11,13 +11,21 @@ import json
 #link_qr = (r_qrcode.json()).get("url")
 link_qr = "https://www.caliel.fr/book/liste"
 
+try:
+    lastmail = json.load(open('lastmail.json', 'r'))
+except FileNotFoundError:
+    lastmail = {}  
+newlastmail = {}
 with open('loggers.json', 'r') as file:
     wait = random.randint(1, 2*60)
     data = json.load(file)
     for item in data:
         if is_today_in_list(item["days"]):
             cookies = {'REMEMBERME':item["cookie"]} 
-            time.sleep(wait)
+            #time.sleep(wait)
+            if not lastmail.get(item["id"]):
+                lastmail[item["id"]] = ""
             r = requests.get(link_qr,cookies=cookies)
-            #send_email(r.text,item["mail"])
+            newlastmail[item["id"]] = send_email(r.text,item["mail"],lastmail[item["id"]])
             writelog(item["id"],r.status_code)
+json.dump(newlastmail, open('lastmail.json', 'w'), indent=2)
--- emailsender.py
+++ emailsender.py
@@ -1,16 +1,20 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.utils import make_msgid
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-def send_email(body,receivermail):
+def send_email(body,receivermail,lastmail):
     message = MIMEMultipart()
     message['From'] = os.getenv('SENDER_MAIL')
     message['To'] = receivermail
     message['Subject'] = "Passage Caliel"
     message.attach(MIMEText(body, 'html'))  # Use 'html' for HTML content
+    if lastmail:
+        message['In-Reply-To'] = lastmail
+        message['References'] = lastmail
     # Connect to the SMTP server
     with smtplib.SMTP(os.getenv('SENDER_SMTP'), 587) as server:
         # Start the TLS connection
@@ -18,5 +22,8 @@ def send_email(body,receivermail):
         # Login to the email account
         server.login(os.getenv('SENDER_MAIL'), os.getenv('SENDER_PASS'))
         # Send the email
+        message["Message-ID"]=make_msgid()
         server.sendmail(os.getenv('SENDER_MAIL'), receivermail, message.as_string())
-    print("Email sent successfully!")
+        print("Email sent successfully!")
+        return message['Message-ID']
+        
